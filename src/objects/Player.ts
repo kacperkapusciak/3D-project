b449539fa7dcf494d0e7c8dbfd0c4fd008b646f7
kacperkapusciak <kapusciak.k@gmail.com>
2020-10-29 18:19:40 +0100

    Add mouse camera support

--- src/objects/Player.ts
+++ src/objects/Player.ts
@@ -3,37 +3,40 @@ import * as THREE from 'three';
 export class Player extends THREE.PerspectiveCamera {
   constructor() {
     super(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    this.registerKeyboard();
+    this.register();
 
-    this.position.set(0, this.height, -5);
+    this.position.set(0, this.height, 0);
+    this.rotation.order = 'YXZ';
   }
 
   private height = 1.8;
   private speed = 0.2;
 
-  private registerKeyboard() {
-    window.addEventListener('keydown', (event) => {
-      this.move(event);
+  private register = () => {
+    document.addEventListener('keydown', (event) => {
+      this.movePlayer(event);
     });
-  }
 
-  private move(event: KeyboardEvent) {
+    document.addEventListener('pointerlockchange', this.lockMouse, false);
+  };
+
+  private movePlayer = (event: KeyboardEvent) => {
     switch (event.code) {
       case 'KeyW':
-        this.position.x += Math.sin(this.rotation.y) * this.speed;
+        this.position.x += Math.sin(-this.rotation.y) * this.speed;
         this.position.z += -Math.cos(this.rotation.y) * this.speed;
         break;
       case 'KeyS':
-        this.position.x -= Math.sin(this.rotation.y) * this.speed;
+        this.position.x -= Math.sin(-this.rotation.y) * this.speed;
         this.position.z -= -Math.cos(this.rotation.y) * this.speed;
         break;
       case 'KeyA':
         this.position.x += Math.sin(this.rotation.y - Math.PI / 2) * this.speed;
-        this.position.z += -Math.cos(this.rotation.y - Math.PI / 2) * this.speed;
+        this.position.z += -Math.cos(this.rotation.y + Math.PI / 2) * this.speed;
         break;
       case 'KeyD':
         this.position.x += Math.sin(this.rotation.y + Math.PI / 2) * this.speed;
-        this.position.z += -Math.cos(this.rotation.y + Math.PI / 2) * this.speed;
+        this.position.z += -Math.cos(this.rotation.y - Math.PI / 2) * this.speed;
         break;
       case 'Space':
         this.position.y += this.speed;
@@ -42,5 +45,18 @@ export class Player extends THREE.PerspectiveCamera {
         this.position.y -= this.speed;
         break;
     }
-  }
+  };
+
+  private lockMouse = () => {
+    if (document.pointerLockElement) {
+      document.addEventListener('mousemove', this.moveView, false);
+    } else {
+      document.removeEventListener('mousemove', this.moveView, false);
+    }
+  };
+
+  private moveView = (event: MouseEvent) => {
+    this.rotation.x -= event.movementY / 200;
+    this.rotation.y -= event.movementX / 200;
+  };
 }
