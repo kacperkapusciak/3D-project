4b707052292138ce6a04e8385d333d5802df3829
kacperkapusciak <kapusciak.k@gmail.com>
2020-10-31 14:09:32 +0100

    Update palette

--- src/palette.ts
+++ src/palette.ts
@@ -1,12 +1,55 @@
 const palette = {
   green: {
-    light: 0x008046,
-    dark: 0x024a33,
+    '200': 0xc3d368,
+    '300': 0xb0c151,
+    '500': 0x99d468,
+    '700': 0x82c250,
+    '900': 0x259d54,
   },
-  white: 0xf0f4eb,
-  brown: 0xb17339,
-  blue: 0x178afc,
-  background: 0xe5e5e5,
+  brown: {
+    '200': 0xa5766f,
+    '500': 0x67493e,
+    '700': 0x3c2c25,
+    '900': 0x28211d,
+  },
+  red: {
+    '500': 0xed5f55,
+    '700': 0xd94c43,
+  },
+  orange: {
+    '500': 0xfab153,
+    '700': 0xf49b41,
+  },
+  yellow: {
+    '500': 0xfcce54,
+    '700': 0xf5ba42,
+  },
+  cyan: {
+    '500': 0x47cec0,
+    '700': 0x3abdaf,
+  },
+  blue: {
+    '300': 0x4ec2e7,
+    '400': 0x3bb1d9,
+    '500': 0x5c9ded,
+    '700': 0x4a8bdb,
+  },
+  purple: {
+    '300': 0xcb93ec,
+    '500': 0xb377d7,
+  },
+  pink: {
+    '500': 0xec87bf,
+    '700': 0xd870ad,
+  },
+  gray: {
+    '100': 0xf1f2f4,
+    '200': 0xe0e4e7,
+    '400': 0xc5ccd4,
+    '500': 0xa5adb8,
+    '700': 0x606873,
+    '900': 0x3e4750,
+  },
 };
 
 export default palette;
