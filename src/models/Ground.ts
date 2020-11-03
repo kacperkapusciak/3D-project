a290945739863320c44d9343850a21555c4bec58
kacperkapusciak <kapusciak.k@gmail.com>
2020-11-03 11:26:04 +0100

    Change ground from plane to box

--- src/models/Ground.ts
+++ src/models/Ground.ts
@@ -1,15 +1,11 @@
 import * as THREE from 'three';
+import { Box } from '../geometry';
 import palette from '../palette';
 
 export class Ground extends THREE.Object3D {
   constructor() {
     super();
-    const geometry = new THREE.PlaneGeometry(40, 40);
-    const material = new THREE.MeshStandardMaterial({ color: palette.orange[500] });
-    const ground = new THREE.Mesh(geometry, material);
-    ground.castShadow = true;
-    ground.receiveShadow = true;
-
+    const ground = new Box(32, 32, 0.4, palette.orange[500], 0, 0.2, 0);
     ground.rotation.x -= Math.PI / 2;
 
     return ground;
